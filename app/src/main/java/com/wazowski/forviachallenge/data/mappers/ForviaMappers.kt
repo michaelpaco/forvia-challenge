e3ce5bdf15ee8b8c0fc88830cb9c0501d6d8113b
Michael <michael.paco.alves@gmail.com>
2024-05-07 23:53:54 +0100

    feat(data): create mapper to map dto into forvia app info list

---
+++ app/src/main/java/com/wazowski/forviachallenge/data/mappers/ForviaMappers.kt
@@ -0,0 +1,25 @@
+package com.wazowski.forviachallenge.data.mappers
+
+import com.wazowski.forviachallenge.data.remote.*
+import com.wazowski.forviachallenge.domain.forvia.ForviaApp
+
+fun Datasets.toForviaAppList(): List<ForviaApp> {
+    return this.all.data.list.map { data ->
+        ForviaApp(
+            id = data.id,
+            name = data.name,
+            packageName = data.packageName,
+            storeId = data.storeId,
+            storeName = data.storeName,
+            vername = data.vername,
+            vercode = data.vercode,
+            downloads = data.downloads,
+            added = data.added,
+            modified = data.modified,
+            updated = data.updated,
+            rating = data.rating,
+            icon = data.icon,
+            graphic = data.graphic,
+        )
+    }
+}
