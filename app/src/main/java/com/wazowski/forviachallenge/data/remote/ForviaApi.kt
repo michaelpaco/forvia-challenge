d57d76d5434498f1d83eff08f7da46af94946fa2
Michael <michael.paco.alves@gmail.com>
2024-05-07 23:51:22 +0100

    feat(api): fetch app list from forvia listApps endpoint

---
+++ app/src/main/java/com/wazowski/forviachallenge/data/remote/ForviaApi.kt
@@ -0,0 +1,11 @@
+package com.wazowski.forviachallenge.data.remote
+
+import retrofit2.http.*
+
+interface ForviaApi {
+
+    @GET("/api/6/listApps")
+    suspend fun getAppsList(
+        @Query("offset") offset: Int
+    ) : ForviaDatasetDto
+}
