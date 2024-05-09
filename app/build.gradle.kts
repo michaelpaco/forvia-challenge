33e2f89542b2469ae899464c5631939fa4bbd024
Michael <michael.paco.alves@gmail.com>
2024-05-09 17:29:39 +0100

    chore(app): improve build gradle versioning and add room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.wazowski.forviachallenge.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -59,39 +59,77 @@ kapt {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
+    val hilt_version = "2.51.1"
+    val room_version = "2.6.1"
+    val moshi_version = "1.15.1"
+    val retrofit_version = "2.11.0"
+    val compose_bom_version = "2024.05.00"
+    val hilt_compiler_version = "1.2.0"
+    val moshi_converter_version = "2.9.0"
+    val hilt_navigation_compose_version = "1.2.0"
 
+    // Core
     implementation("androidx.core:core-ktx:1.13.1")
+
+    // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    // Compose
+    implementation(platform("androidx.compose:compose-bom:$compose_bom_version"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-runtime-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Http
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-moshi:$moshi_converter_version")
+    implementation("com.squareup.moshi:moshi-kotlin:$moshi_version")
+    //noinspection KaptUsageInsteadOfKsp
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi_version")
+
+    // Image loading
     implementation("io.coil-kt:coil-compose:2.6.0")
 
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-compiler:2.51.1")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
+    // Room
+    implementation("androidx.room:room-runtime:$room_version")
     //noinspection KaptUsageInsteadOfKsp
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
+    kapt("androidx.room:room-compiler:$room_version")
 
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // DI
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    implementation("androidx.hilt:hilt-navigation-compose:$hilt_navigation_compose_version")
+    kapt("com.google.dagger:hilt-compiler:$hilt_version")
+    kapt("androidx.hilt:hilt-compiler:$hilt_compiler_version")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:4.8.0")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+
+
+    // For instrumented tests.
+    androidTestImplementation("com.google.dagger:hilt-android-testing:$hilt_version")
+    // ...with Kotlin.
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:$hilt_version")
+    // ...with Java.
+    androidTestAnnotationProcessor("com.google.dagger:hilt-android-compiler:$hilt_version")
 }
