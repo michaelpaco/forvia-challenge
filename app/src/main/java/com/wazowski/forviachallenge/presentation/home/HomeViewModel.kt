588704aa923a1129aa199784df5adaad0b2612e4
Michael <michael.paco.alves@gmail.com>
2024-05-09 23:32:30 +0100

    feat(connectivity): add permission ACCESS_NETWORK_STATE

--- app/src/main/java/com/wazowski/forviachallenge/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/wazowski/forviachallenge/presentation/home/HomeViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import com.wazowski.forviachallenge.presentation.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel : ViewModel() {
+class HomeViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
     val uiState: StateFlow<UiState> = _uiState
 
