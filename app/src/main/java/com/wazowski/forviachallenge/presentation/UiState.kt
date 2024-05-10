0ca7f6bf7ca12ae51920b1fb3b509c11b0fcf3b6
Michael <michael.paco.alves@gmail.com>
2024-05-10 11:45:15 +0100

    feat(uistate): add new state topdownloaded apps to uistate class

--- app/src/main/java/com/wazowski/forviachallenge/presentation/UiState.kt
+++ app/src/main/java/com/wazowski/forviachallenge/presentation/UiState.kt
@@ -3,10 +3,11 @@ package com.wazowski.forviachallenge.presentation
 import com.wazowski.forviachallenge.domain.model.ForviaApp
 
 sealed class UiState {
+    data object Empty : UiState()
     data object Loading : UiState()
-    data class Success(
-        val allApps: List<ForviaApp>
-    ) : UiState()
-
     data class Error(val message: String) : UiState()
+    data class Success(
+        val allApps: List<ForviaApp> = emptyList(),
+        val topDownloadedApps: List<ForviaApp> = emptyList()
+    ) : UiState()
 }
