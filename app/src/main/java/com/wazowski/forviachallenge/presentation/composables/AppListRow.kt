680ad0ae2064de9ec8eebb792552e006fb453c60
Michael <michael.paco.alves@gmail.com>
2024-05-12 02:47:09 +0100

    feat(list): add icon to see more apps

--- app/src/main/java/com/wazowski/forviachallenge/presentation/composables/AppListRow.kt
+++ app/src/main/java/com/wazowski/forviachallenge/presentation/composables/AppListRow.kt
@@ -15,74 +15,51 @@ import com.wazowski.forviachallenge.common.*
 import com.wazowski.forviachallenge.common.Constants.PADDING_M
 import com.wazowski.forviachallenge.common.Constants.PADDING_XL
 import com.wazowski.forviachallenge.common.Constants.PADDING_XS
+import com.wazowski.forviachallenge.common.Constants.POPULAR_APPS_TITLE
 import com.wazowski.forviachallenge.domain.model.ForviaApp
 import com.wazowski.forviachallenge.presentation.theme.ForviaChallengeTheme
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
 
-@OptIn(FlowPreview::class)
 @Composable
 fun AppListRow(
     apps: List<ForviaApp>,
     modifier: Modifier = Modifier,
     title: String = "",
-    onFirstVisibleItemChange: ((ForviaApp) -> Unit)? = null,
     onSeeMoreApps: (() -> Unit)? = null,
     content: @Composable (ForviaApp) -> Unit
 ) {
-    val isInPopularList = title == "Popular"
-    val scrollState = rememberLazyListState()
-    var visibleItemIndex: Int
-
-    LaunchedEffect(key1 = scrollState) {
-        if (onFirstVisibleItemChange == null) return@LaunchedEffect
-
-        snapshotFlow {
-            scrollState.layoutInfo
-        }.debounce(500L).collectLatest { layoutInfo ->
-
-            visibleItemIndex = if (layoutInfo.visibleItemsInfo.size > 2) {
-                layoutInfo.visibleItemsInfo[1].index
-            } else {
-                layoutInfo.visibleItemsInfo[0].index
-            }
-
-            onFirstVisibleItemChange.invoke(apps[visibleItemIndex])
-        }
-    }
+    val hasBackground = title == POPULAR_APPS_TITLE || title == ""
 
     Column(
         modifier = modifier
     ) {
         if (title.isNotEmpty()) Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.padding(
                 start = PADDING_XL.dp, bottom = PADDING_M.dp
-            )
+            ).fillMaxWidth()
         ) {
             Text(
                 title,
                 style = MaterialTheme.typography.titleLarge,
                 fontWeight = FontWeight.Bold,
-                color = if (isInPopularList) Color.White else Color.Black
+                color = if (hasBackground) Color.White else Color.Black
             )
+
+            IconButton(onClick = { onSeeMoreApps?.invoke() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+                    contentDescription = Icons.AutoMirrored.Filled.ArrowForwardIos.name
+                )
+            }
         }
         LazyRow(
-            state = scrollState,
             contentPadding = PaddingValues(horizontal = PADDING_M.dp, vertical = PADDING_XS.dp),
             horizontalArrangement = Arrangement.spacedBy(12.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             items(count = apps.size) {
                 content(apps[it])
-            }
-
-            item {
-                IconButton(onClick = { onSeeMoreApps?.invoke() }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
-                        contentDescription = "More"
-                    )
-                }
             }
         }
     }
