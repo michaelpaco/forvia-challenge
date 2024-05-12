f9780d842ec26486f0a60701c08bb953090aa84e
Michael <michael.paco.alves@gmail.com>
2024-05-12 23:22:37 +0100

    feat(theme): create custom colors for card list and download button

--- app/src/main/java/com/wazowski/forviachallenge/presentation/theme/CustomPalette.kt
+++ app/src/main/java/com/wazowski/forviachallenge/presentation/theme/CustomPalette.kt
@@ -5,19 +5,34 @@ import androidx.compose.ui.graphics.Color
 
 @Immutable
 data class CustomPalette(
-    val extraColor1: Color = Color.Unspecified,
+    val listBackground: Color = Color.Unspecified,
+    val buttonBackground: Color = Color.Unspecified,
+    val metadataColor: Color = Color.Unspecified,
+    val metadataBackgroundColor: Color = Color.Unspecified,
 )
 
-val LightExtraColor1 = Color(color = 0xFFFA5825)
+val ListBackgroundLight = Color(color = 0xFFFA5825)
+val ButtonBackgroundLight = Color(color = 0xFF0C1932)
+val MetadataColorLight = Color(color = 0xFFFA253D)
+val MetadataBackgroundLight = Color(color = 0xFFFFC4B2)
 
-val DarkExtraColor1 = Color(color = 0xFFFA5825)
+val ListBackgroundDark = Color(color = 0xFFFA5825)
+val ButtonBackgroundDark = Color(color = 0xFF0C1932)
+val MetadataColorDark = Color(color = 0xFFFA253D)
+val MetadataBackgroundDark = Color(color = 0xFFFFC4B2)
 
 val LightCustomPalette = CustomPalette(
-    extraColor1 = LightExtraColor1
+    listBackground = ListBackgroundLight,
+    buttonBackground = ButtonBackgroundLight,
+    metadataColor = MetadataColorLight,
+    metadataBackgroundColor = MetadataBackgroundLight
 )
 
 val DarkCustomPalette = CustomPalette(
-    extraColor1 = DarkExtraColor1
+    listBackground = ListBackgroundDark,
+    buttonBackground = ButtonBackgroundDark,
+    metadataColor = MetadataColorDark,
+    metadataBackgroundColor = MetadataBackgroundDark
 )
 
 val LocalCustomPalette = staticCompositionLocalOf { CustomPalette() }
